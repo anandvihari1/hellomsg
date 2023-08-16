65033e32e5c6ca820bab4b48e10d7f26c796e417
Anand Padamala <padamala.anand001@gmail.com>
2023-08-16 12:39:14 +0530

    first api

--- src/main/java/com/hellomsg/com/HelloApplication.java
+++ src/main/java/com/hellomsg/com/HelloApplication.java
@@ -2,10 +2,18 @@ package com.hellomsg.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HelloApplication {
 
+	@GetMapping("/getmsg")
+	public String getMsg()
+	{
+		return "Hello Aws !!!!!";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(HelloApplication.class, args);
 	}
